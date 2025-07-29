e5621169615077ad002938318fd818513b810120
cschaus1507 <christopher.schaus@warlocks1507.com>
2025-07-29 17:25:56 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        rhPurple: '#5e2a84',
+        rhGray: '#6b7280',
+      }
+    },
+  },
+  plugins: [],
+}
