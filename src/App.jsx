acca80e5ce76f4105f5922eaf3fe1edc21b15d3e
cschaus1507 <christopher.schaus@warlocks1507.com>
2025-07-29 18:15:58 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Dashboard from './pages/Dashboard';
 import AssignedDevices from './pages/AssignedDevices';
 import AvailableDevices from './pages/AvailableDevices';
@@ -8,13 +9,16 @@ import DeviceOverview from './pages/DeviceOverview';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/assigned" element={<AssignedDevices />} />
-      <Route path="/available" element={<AvailableDevices />} />
-      <Route path="/add-student" element={<AddStudent />} />
-      <Route path="/repair" element={<RepairPool />} />
-      <Route path="/device/:tag" element={<DeviceOverview />} />
-    </Routes>
+    <div className="p-6">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/assigned" element={<AssignedDevices />} />
+        <Route path="/available" element={<AvailableDevices />} />
+        <Route path="/add-student" element={<AddStudent />} />
+        <Route path="/repair" element={<RepairPool />} />
+        <Route path="/device/:tag" element={<DeviceOverview />} />
+      </Routes>
+    </div>
   );
 }
