b27641b6c0ad89afb379bb67d54baf3380cbb1a0
cschaus1507 <christopher.schaus@warlocks1507.com>
2025-07-29 17:58:45 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import DeviceList from './components/DeviceList';
+
+export default function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-rhPurple">Royalton-Hartland Chromebook Tracker</h1>
+      <DeviceList />
+    </div>
+  );
+}
