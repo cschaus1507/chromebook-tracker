e1255514d68f9331784af89430fdc1c810bbb3da
cschaus1507 <christopher.schaus@warlocks1507.com>
2025-07-29 19:03:15 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
         rhPurple: '#5e2a84',
         rhGray: '#6b7280',
       }
-    },
+    }
   },
   plugins: [],
-};
+}
